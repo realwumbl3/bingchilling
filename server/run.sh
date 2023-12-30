4dea1fee8426ce47b5c50254681a5199567d3bad
wumbl3 <wumbl3@gmail.com>
2023-12-30 02:39:49 -0500

    remove extra files

--- server/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -x
-app="/home/wumbl3vps/Dev/bingchilling-game"
-cd $app
-source pyvenv/bin/activate
-gunicorn --error-logfile "${app}/server/log/gunicorn/error.log" --access-logfile "${app}/server/log/gunicorn/access.log" --capture-output --log-level debug -k eventlet -w 1 --bind unix:"${app}/server/sock.sock" -m 007 app:app
